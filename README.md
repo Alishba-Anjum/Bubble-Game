36c0e33907f57a040aa6d2c402ce48594e25f06a
Alishba-Anjum <alishbaahmedkhan11@gmail.com>
2024-04-30 15:55:16 +0500

    commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-"# Bubble-Game" 
+ # Bubble Clicker Game
+
+Bubble Clicker Game is a simple web-based game where players need to click on bubbles containing randomly generated numbers within a specified time limit to score points.
+
+## Features
+
+- Generate bubbles with random numbers.
+- Generate a random "hit" number for the player to aim for.
+- Start a countdown timer.
+- Increase the score when the player clicks on the correct bubble.
+- Display the final score when the timer runs out.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## How to Play
+
+1. Open the `index.html` file in your web browser.
+2. Click on bubbles containing the randomly generated "hit" number within the specified time limit.
+3. Score points for each correct click.
+4. The game ends when the timer runs out, and your final score is displayed.
+
---
+++ main.js
@@ -0,0 +1,53 @@
+let bubbles = "";
+let timer = 30;
+let score = 0;
+let hitrn = 0;
+
+
+
+function makeBubbles() {
+
+  for (let i = 1; i <= 108; i++) {
+    let number = Math.floor(Math.random() * 10);
+    bubbles += `<div class="bubble">${number}</div>`;
+  }
+  document.querySelector(".bottom").innerHTML = bubbles;
+}
+let hit = document.querySelector("#hitval");
+function getHit() {
+   hitrn = Math.floor(Math.random() * 10);
+ hit.textContent = hitrn;
+}
+
+function runTimer() {
+  let intTimer = setInterval(function () {
+    if (timer > 0) {
+      timer--;
+      document.querySelector("#timerval").textContent = timer;
+    } else {
+        bottomBubble.innerHTML =`<h1> Game over! <br> your score is "${score}"</h1>`;
+      clearInterval(intTimer);
+    }
+  }, 1000);
+}
+
+let incScore = document.querySelector("#scoreval");
+function increaseScore(){
+ incScore.textContent = score;
+ score += 10;
+}
+
+let bottomBubble = document.querySelector(".bottom");
+bottomBubble.addEventListener("click" , function (dets){
+  let clickedNumber = Number(dets.target.textContent);
+  if(clickedNumber === hitrn){
+    increaseScore();
+    makeBubbles();
+    getHit();
+  }
+})
+
+getHit();
+runTimer();
+makeBubbles();
+increaseScore();
